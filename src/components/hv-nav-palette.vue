328731c641823df353cf3013d646f32542e57199
Matheus Alves <mtzin44@gmail.com>
2018-03-21 09:08:29 -0300

    added rgba in palette

--- src/components/hv-nav-palette.vue
+++ src/components/hv-nav-palette.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="palette">
     <ul class="color-container">
-      <li class="color-box" v-for="color in palette" :key="color"
+      <li class="color-box" v-for="color in palette" :key="color.hex"
         @click="handlerClick(color)"
-        :style="{background: color}">
+        :style="{background: color.hex}">
         <div class="color-selected" v-show="color === selectedColor"></div>
       </li>
     </ul>
@@ -20,19 +20,31 @@ export default {
     return {
       selectedColor: null,
       palette: [
-        '#4D4D4D', '#999999', '#FFFFFF', '#F44E3B', '#FE9200', '#FCDC00',
-        '#DBDF00', '#A4DD00', '#68CCCA', '#73D8FF', '#AEA1FF', '#FDA1FF',
-        '#333333', '#808080', '#CCCCCC', '#D33115', '#E27300', '#FCC400',
-        '#B0BC00', '#68BC00', '#16A5A5', '#009CE0', '#7B64FF', '#FA28FF',
-        '#000000', '#666666', '#B3B3B3', '#9F0500', '#C45100', '#FB9E00',
-        '#808900', '#194D33', '#0C797D', '#0062B1', '#653294', '#AB149E'
+        {hex: '#4D4D4D', rgba: 'rgba(77,77,77, 0.5)'}, {hex: '#999999', rgba: 'rgba(153,153,153, 0.5)'},
+        {hex: '#FFFFFF', rgba: 'rgba(255,255,255, 0.5)'}, {hex: '#F44E3B', rgba: 'rgba(244,78,59, 0.5)'},
+        {hex: '#FE9200', rgba: 'rgba(254,146,0, 0.5)'}, {hex: '#FCDC00', rgba: 'rgba(252,220,0, 0.5)'},
+        {hex: '#DBDF00', rgba: 'rgba(219,223,0, 0.5)'}, {hex: '#A4DD00', rgba: 'rgba(164,221,0, 0.5)'},
+        {hex: '#68CCCA', rgba: 'rgba(104,204,202, 0.5)'}, {hex: '#73D8FF', rgba: 'rgba(115,216,255, 0.5)'},
+        {hex: '#AEA1FF', rgba: 'rgba(174,161,255, 0.5)'}, {hex: '#FDA1FF', rgba: 'rgba(253,161,255, 0.5)'},
+        {hex: '#333333', rgba: 'rgba(51,51,51, 0.5)'}, {hex: '#808080', rgba: 'rgba(128,128,128, 0.5)'},
+        {hex: '#CCCCCC', rgba: 'rgba(204,204,204, 0.5)'}, {hex: '#D33115', rgba: 'rgba(211,49,21, 0.5)'},
+        {hex: '#E27300', rgba: 'rgba(226,115,0, 0.5)'}, {hex: '#FCC400', rgba: 'rgba(252,196,0, 0.5)'},
+        {hex: '#B0BC00', rgba: 'rgba(176,188,0, 0.5)'}, {hex: '#68BC00', rgba: 'rgba(104,188,0, 0.5)'},
+        {hex: '#16A5A5', rgba: 'rgba(22,165,165, 0.5)'}, {hex: '#009CE0', rgba: 'rgba(0,156,224, 0.5)'},
+        {hex: '#7B64FF', rgba: 'rgba(123,100,255, 0.5)'}, {hex: '#FA28FF', rgba: 'rgba(250,40,255, 0.5)'},
+        {hex: '#000000', rgba: 'rgba(0,0,0, 0.5)'}, {hex: '#666666', rgba: 'rgba(102,102,102, 0.5)'},
+        {hex: '#B3B3B3', rgba: 'rgba(179,179,179, 0.5)'}, {hex: '#9F0500', rgba: 'rgba(159,5,0, 0.5)'},
+        {hex: '#C45100', rgba: 'rgba(196,81,0, 0.5)'}, {hex: '#FB9E00', rgba: 'rgba(251,158,0, 0.5)'},
+        {hex: '#808900', rgba: 'rgba(128,137,0, 0.5)'}, {hex: '#194D33', rgba: 'rgba(25,77,51, 0.5)'},
+        {hex: '#0C797D', rgba: 'rgba(12,121,125, 0.5)'}, {hex: '#0062B1', rgba: 'rgba(0,98,177, 0.5)'},
+        {hex: '#653294', rgba: 'rgba(101,50,148, 0.5)'}, {hex: '#AB149E', rgba: 'rgba(171,20,158, 0.5)'}
       ]
     }
   },
   methods: {
     handlerClick (color) {
-      this.selectedColor = color
-      const fill = new ol_style.Fill({ color }) // fundo
+      this.selectedColor = color.hex
+      const fill = new ol_style.Fill({ color: color.rgba }) // fundo
       const stroke = new ol_style.Stroke({ color: '#DAE6F0', width: 1.25 }) // contorno
       const new_style = new ol_style.Style({ fill, stroke })
       this.layer.vector_layer.setStyle(new_style)
